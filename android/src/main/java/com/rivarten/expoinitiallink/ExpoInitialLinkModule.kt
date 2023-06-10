f6acb66030dc6ddbb53c5cf5dd17962a8786edce
rivarten <508limit@gmail.com>
2023-06-10 13:08:07 +0900

    chore: android remove view files

--- android/src/main/java/com/rivarten/expoinitiallink/ExpoInitialLinkModule.kt
+++ android/src/main/java/com/rivarten/expoinitiallink/ExpoInitialLinkModule.kt
@@ -34,14 +34,5 @@ class ExpoInitialLinkModule : Module() {
         "value" to value
       ))
     }
-
-    // Enables the module to be used as a native view. Definition components that are accepted as part of
-    // the view definition: Prop, Events.
-    View(ExpoInitialLinkView::class) {
-      // Defines a setter for the `name` prop.
-      Prop("name") { view: ExpoInitialLinkView, prop: String ->
-        println(prop)
-      }
-    }
   }
 }
