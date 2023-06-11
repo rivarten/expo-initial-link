8e9b4625e6fc7bf7a24632f97c6830f91b19e804
malimos-sogo <sogo@malimos.jp>
2023-06-11 21:52:40 +0900

    chore: remove unnecessary

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 // Import the native module. On web, it will be resolved to ExpoInitialLink.web.ts
 // and on native platforms to ExpoInitialLink.ts
-import { ChangeEventPayload } from "./ExpoInitialLink.types";
 import ExpoInitialLinkModule from "./ExpoInitialLinkModule";
 
 export function isActivatedByDeepLink(): boolean {
   return ExpoInitialLinkModule.isActivatedByDeepLink();
 }
-
-export { ChangeEventPayload };
