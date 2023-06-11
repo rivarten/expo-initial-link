ff839a598fb273d625f17bbd7bdc5f936be95671
malimos-sogo <sogo@malimos.jp>
2023-06-11 21:06:45 +0900

    chore: remove unnecessary

--- android/src/main/java/com/rivarten/expoinitiallink/ExpoInitialLinkModule.kt
+++ android/src/main/java/com/rivarten/expoinitiallink/ExpoInitialLinkModule.kt
@@ -18,31 +18,10 @@ class ExpoInitialLinkModule : Module(),Package {
     // The module will be accessible from `requireNativeModule('ExpoInitialLink')` in JavaScript.
     Name("ExpoInitialLink")
 
-    // Sets constant properties on the module. Can take a dictionary or a closure that returns a dictionary.
-    Constants(
-      "PI" to Math.PI
-    )
-
-    // Defines event names that the module can send to JavaScript.
-    Events("onChange")
-
-    // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
-    Function("hello") {
-      "Hello world! 👋"
-    }
-
     Function("isActivatedByDeepLink") {
       return@Function getPreferences().getBoolean("isActivatedByDeepLink", false)
     }
 
-    // Defines a JavaScript function that always returns a Promise and whose native code
-    // is by default dispatched on the different thread than the JavaScript runtime runs on.
-    AsyncFunction("setValueAsync") { value: String ->
-      // Send an event to JavaScript.
-      sendEvent("onChange", mapOf(
-        "value" to value
-      ))
-    }
   }
 
   private val context
